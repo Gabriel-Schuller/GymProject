763a230df1686ebe34a33f8b43358171bb4c12ec
antico94 <meiuandrei@gmail.com>
2021-11-18 21:00:18 +0200

    working on muscle selector

--- GymProject/wwwroot/js/site.js
+++ GymProject/wwwroot/js/site.js
@@ -1,11 +1,1 @@
-﻿// $('.col-md-4').on('click', (event)=> {
-
-// let cards = $('.col-md-4')
-// for (let i=0; i<cards.length; i++){
-//     cards[i].addEventListener('click', function () {CardClick(cards[i].id)})
-// }
-//
-// function CardClick(id){
-//     console.log(id)
-// }
-
+﻿
